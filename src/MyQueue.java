197df51c285cc2c73b32ddc10f2a7a8da9684873
Stefan1991 <106970205+Chefan69@users.noreply.github.com>
2024-01-21 21:58:58 +0100

    Update MyQueue.java

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,143 +1,62 @@
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
-
-public class MyQueue implements Queue<Integer> {
-    protected final int SIZE;
-    protected Queue<Integer> queue;
+public class MyQueue {
+    private final int SIZE;
+    private int[] queue;
+    private int front, rear;
 
     public MyQueue(int SIZE) {
         this.SIZE = SIZE;
-        queue = new LinkedList<>();
+        queue = new int[SIZE];
+        front = -1;
+        rear = -1;
     }
 
-    @Override
-    public int size() {
-        return queue.size();
+    // check if the queue is full
+    public boolean isFull() {
+        return ((rear + 1) % SIZE == front);
     }
 
-    @Override
+    // check if the queue is empty
     public boolean isEmpty() {
-        return queue.isEmpty();
+        return (front == -1);
     }
 
-    @Override
-    public boolean contains(Object o) {
-        return queue.contains(o);
-    }
-
-    @Override
-    public Iterator<Integer> iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(Integer integer) {
-        if (size() == SIZE) {
-            System.out.println("Queue is full. Cannot add element: " + integer);
-            return false;
+    // insert elements to the queue
+    public void enqueue(int element) {
+        if (isFull()) {
+            System.out.println("Queue is full");
         } else {
-            return queue.add(integer);
+            if (front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % SIZE;
+            queue[rear] = element;
         }
     }
 
-    @Override
-    public boolean offer(Integer integer) {
-        if (size() == SIZE) {
-            System.out.println("Queue is full. Cannot offer element: " + integer);
-            return false;
-        } else {
-            return queue.offer(integer);
-        }
-    }
-
-    @Override
-    public Integer remove() {
+    // remove and return the front element from the queue
+    public int dequeue() {
         if (isEmpty()) {
-            System.out.println("Queue is empty. Cannot remove.");
-            return null;
+            System.out.println("Queue is empty");
+            return -1;
         } else {
-            return queue.remove();
+            int element = queue[front];
+            if (front == rear) {
+                front = -1;
+                rear = -1;
+            } else {
+                front = (front + 1) % SIZE;
+            }
+            return element;
         }
     }
 
-    @Override
-    public Integer poll() {
+    // return the front element of the queue
+    public int peek() {
         if (isEmpty()) {
-            System.out.println("Queue is empty. Cannot poll.");
-            return null;
+            System.out.println("Queue is empty");
+            return -1;
         } else {
-            return queue.poll();
+            return queue[front];
         }
-    }
-
-    @Override
-    public Integer element() {
-        if (isEmpty()) {
-            System.out.println("Queue is empty. No element to retrieve.");
-            return null;
-        } else {
-            return queue.element();
-        }
-    }
-
-    @Override
-    public Integer peek() {
-        if (isEmpty()) {
-            System.out.println("Queue is empty. No element to peek.");
-            return null;
-        } else {
-            return queue.peek();
-        }
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return queue.remove(o);
-    }
-
-    @Override
-    public boolean containsAll(java.util.Collection<?> collection) {
-        return queue.containsAll(collection);
-    }
-
-    @Override
-    public boolean addAll(java.util.Collection<? extends Integer> collection) {
-        if (size() + collection.size() > SIZE) {
-            System.out.println("Adding all elements will exceed the queue size.");
-            return false;
-        } else {
-            return queue.addAll(collection);
-        }
-    }
-
-    @Override
-    public boolean removeAll(java.util.Collection<?> collection) {
-        return queue.removeAll(collection);
-    }
-
-    @Override
-    public boolean retainAll(java.util.Collection<?> collection) {
-        return queue.retainAll(collection);
-    }
-
-    @Override
-    public void clear() {
-        queue.clear();
-    }
-
-    @Override
-    public String toString() {
-        return "MyQueue: " + queue.toString();
     }
 }
